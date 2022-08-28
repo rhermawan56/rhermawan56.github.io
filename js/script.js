41d04a345685e95e635fc2c3aafa4309c3a9a2d6
Rizky Hermawan <rhermawan56@gmail.com>
2022-08-28 23:19:08 +0700

    animate added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-let about_p = $('.about p')
 function about_text() {
-    let about_p = $('.about p')
+    let about_p = $('.about .text p')
     let index = about_p.length - 1
 
     let p = setInterval(() => {
@@ -9,11 +8,88 @@ function about_text() {
         index --
         if (index == -1 ) {
             clearInterval(p)
+            $('<hr>').insertAfter('.about .text')
+            $('.about .contain').css('transform', 'scale(1)')
+            $('.result').css('transform', 'scale(1)')
         }
     }, 1000);
 }
 
+function aboutBg() {
+    for (let index = 0; index < 3; index++) {
+        $('<div class = "img-bg-left p-absolute"></div>').insertAfter('.about .image .img-bg-2')
+        $('<div class = "img-bg-right p-absolute"></div>').insertAfter('.about .image .img-bg-2')
+    }
+
+    let circ_1 = $('.about .image .img-bg-left')
+    let circ_2 = $('.about .image .img-bg-right')
+    let circ_width = $(circ_1).width() / 16
+
+    for (let index = 0; index < circ_1.length; index++) {
+        if (index == 1) {
+            circ_width = circ_width - 3
+            $(circ_1[index]).css({
+                'width': circ_width + "rem",
+                'height': circ_width + "rem",
+                'left': '15%',
+                'transform': 'translateX(-15%)'
+            })
+
+            $(circ_2[index]).css({
+                'width': circ_width + "rem",
+                'height': circ_width + "rem",
+                'right': '15%',
+                'transform': 'translateX(15%)'
+            })
+        }
+
+        if (index > 1) {
+            circ_width = circ_width - 3
+
+            $(circ_1[index]).css({
+                'width': circ_width + "rem",
+                'height': circ_width + "rem",
+                'left': '6%',
+                'transform': 'translateX(-6%)'
+            })
+
+            $(circ_2[index]).css({
+                'width': circ_width + "rem",
+                'height': circ_width + "rem",
+                'right': '6%',
+                'transform': 'translateX(6%)'
+            })
+        }
+    }
+}
+
+function myResult() {
+    let result_p = $('.result .text p')
+    let index = result_p.length
+    let p = setInterval(() => {
+        $(result_p[index]).addClass('stop-animate')
+        $(result_p[index - 1]).addClass('start-animate')
+        index--
+        if (index == -1) {
+            clearInterval(p)
+            $('.result .contain').addClass('show-scale')
+            $('.result hr').addClass('show-scale')
+        }
+    }, 1000);
+}
+
+$(window).scroll(function () {
+    let check = $('.result .contain').hasClass('show-scale')
+    let height = $(window).scrollTop()
+    if (height > 50 && check != true) {
+        myResult()
+    }
+})
+
 $(function () {
-    console.log(about_p)
+    
     about_text()
+    
+    aboutBg()
+
 })
